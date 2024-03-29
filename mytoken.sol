eb28a68609c0e94125b71057567c789196027e42
Ankittiwari3018 <144475929+Ankittiwari3018@users.noreply.github.com>
2024-03-29 22:21:37 +0530

    Create mytoken.sol

---
+++ mytoken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >= 0.8.4;
+
+contract My_Token {
+   
+  // variables 
+    string public tokenName = “ANKIT;
+    string public tokenAbbry = “ANK”;
+    uint public totalSupply = 1000;
+    
+ // mapping
+    mapping(address => uint) public balances;
+
+ // function to mint tokens
+
+    function burn (address _address, uint _value) public {
+       if (balances[_address] >= _value) {
+          totalSupply -= _value;
+          balances[_address] -= _value;
+       }
+    }     
+
+// function to mint tokens
+
+    function mint (address _address, uint _value) public {
+       totalSupply += _value;
+       balances[_address] += _value;
+    }
+}
